3770afe50b0899cc3e89a96350b882242c12954a
chenry900 <158115154+chenry900@users.noreply.github.com>
2025-04-25 19:44:02 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,25 @@
 function tsp_hk(distance_matrix) {
-    return -1;
+    var visited = [];
+    var start = 0;
+    var answer = 0;
+
+    for (var i = 1; i < distance_matrix.length; i++) {
+        visited[i] = false;
+    }
+
+    answer = recursive(distance_matrix, visited, start, answer);
+    return answer;
+}
+
+function recursive(distance_matrix, visited, start, answer) {
+    var cities = distance_matrix.length;
+
+    if (cities == 2) {
+        return distance_matrix[0][1];
+    }
+    else {
+        
+
+    
+    return answer;
 }
