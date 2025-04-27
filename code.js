73b036c4fb5a07d84b3f1cd3577b3b81aaa78848
chenry900 <158115154+chenry900@users.noreply.github.com>
2025-04-27 15:01:57 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,22 +1,22 @@
 function tsp_hk(distance_matrix) {
-    if(distance_matrix.length < 2) { return 0;}
-
+    if(distance_matrix.length < 2) {return 0;}
+    var cache = [];
     var cities = [];
-    var cache = {};
-
     for (var i = 0; i < distance_matrix.length; i++) {
         cities.push(i);
     }
-    
-    function recursive(cities, start) {
-        if (cities.length == 2) { return distance_matrix[cities[0]][cities[1]];}
-        
-        var key = JSON.stringify(cities.sort());
-        if (cache[key] !== undefined) { 
-            return cache[key];
-        }
 
-        var answer = Infinity;
+    function heldKarp(cities, start) {
+        //if |cities| == 2: return length between those two cities
+        if(cities.length == 2) {return distance_matrix[cities[0]][cities[1]];}
+
+        //Check cache to see if this subset has been processed before
+        var key = JSON.stringify(cities.sort());
+        if(cache[key] === undefined) {cache[key] = [];}
+        if(cache[key][start] !== undefined) {return cache[key][start];}
+ 
+        //Get minimum for held-karp of the rest of the graph
+        var min = Infinity;
         var trimmed = [...cities];
         trimmed.splice(trimmed.indexOf(start), 1);
         for(var i = 0; i < trimmed.length; i++) {
@@ -24,16 +24,14 @@ function tsp_hk(distance_matrix) {
             min = Math.min(min, 
                 heldKarp(trimmed, city) + distance_matrix[start][city]);
         }
-
-            cache[key][start] = answer;
-            return answer;
-        }
-
-        var cost = Infinity;
-        var cities = [];
-        for (var i = 0; i < cities.length; i++) {
-            cost = Math.min(cost, recursive(cities, cities[i]));
-        }
-
-        return cost;
+        cache[key][start] = min;
+        return min;
     }
+
+    //|V| different start positions at beginning
+    var min = Infinity;
+    for(var i = 0; i < cities.length; i++) {
+        min = Math.min(min, heldKarp(cities, cities[i]));
+    }
+    return min;
+}
