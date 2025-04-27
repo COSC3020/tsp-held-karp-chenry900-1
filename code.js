610c4c5fb793cab76f27403bc621bdcb0c3caf41
chenry900 <158115154+chenry900@users.noreply.github.com>
2025-04-27 12:08:51 -0600

    Update code.js

--- code.js
+++ code.js
@@ -3,13 +3,14 @@ function tsp_hk(distance_matrix) {
     var testCost = 0;
     var count = 1;
     var answer = Infinity;
+    var memorize = {};// copilot
 
     for (var startCity = 0; startCity < distance_matrix.length; startCity++) {
         for (var i = 0; i < distance_matrix.length; i++) {
             visited[i] = false;
         }
         visited[startCity] = true;
-        testCost = recursive(distance_matrix, visited, startCity, 0, count);
+        testCost = recursive(distance_matrix, visited, startCity, 0, count, memorize);
         if (answer > testCost) {
             answer = testCost;
         }
@@ -18,11 +19,16 @@ function tsp_hk(distance_matrix) {
     return answer;
 }
 
-function recursive(distance_matrix, visited, city, testCost, count) {
+function recursive(distance_matrix, visited, city, testCost, count, memorize) {
     var cities = distance_matrix.length;
 
     if (count == cities) {
         return testCost;
+    }
+
+    var memorizeKey = visited.join(",") +"-" + city;//copilot
+    if (memorize[memorizeKey] !== undefined) {//copilot
+        return memorize[memorizeKey];//copilot
     }
 
     var answer = Infinity;
@@ -31,13 +37,14 @@ function recursive(distance_matrix, visited, city, testCost, count) {
         if (visited[i] == false) {
             visited[i] = true;
             var addedCost = testCost + distance_matrix[city][i];
-            var totalCost = recursive(distance_matrix, visited, i, addedCost, (count +1));  
+            var totalCost = recursive(distance_matrix, visited, i, addedCost, (count +1), memorize);  
             if (answer > totalCost) {
                 answer = totalCost;
             }
             visited[i] = false;
         }
     }
+    memorize[memorizeKey] = answer; //copilot
 
     return answer;
 }
